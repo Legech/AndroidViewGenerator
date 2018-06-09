ef590de5098c88face651b7d352fd023f7b919d1
YusukeIto <ito@legech.com>
2018-06-09 20:28:21 +0900

    OutputModel Refactor

--- src/main/kotlin/com/legech/android/view/generator/model/OutputModel.kt
+++ src/main/kotlin/com/legech/android/view/generator/model/OutputModel.kt
@@ -7,30 +7,20 @@ import java.io.File
 
 class OutputModel {
 
-    private val outputEntity by lazy {
-        createOutputEntity("/setting.json")
-    }
-
     fun fileOutputExecute() {
+        val outputEntity = createOutputEntity("/setting.json")
         val setting = outputEntity.setting
         outputEntity.uiClasses.forEach {
-            val outSrcFile = File("out/src/")
-            if (!outSrcFile.exists()) {
-                outSrcFile.mkdir()
-            }
-            val outPath = outSrcFile.path + "/" + it.outputType.toLowerCase() + "/"
-            File(outPath).apply {
-                mkdir()
-            }
-            val fileStr = File("$outPath${it.className}${it.outputType}.${setting.fileExt()}").absolutePath
-            val uiFile = File(fileStr).apply {
-                if (exists()) {
-                    delete()
-                }
-            }
+            val outSrcFile = File("out/src/").apply { if (!exists()) mkdir() }
 
             val model = if (it.isActivity()) ActivityOutputModel(it, setting) else FragmentOutputModel(it, setting)
             model.apply {
+                val outPath = outSrcFile.path + "/" + it.outputType.toLowerCase() + "/"
+                File(outPath).apply { mkdir() }
+                val fileStr = File("$outPath${it.className}${it.outputType}.${setting.fileExt()}").absolutePath
+                val uiFile = File(fileStr).apply {
+                    if (exists()) delete()
+                }
                 uiOutput(uiFile)
                 outputPresenter(outSrcFile)
                 outputLayout()
@@ -38,4 +28,3 @@ class OutputModel {
         }
     }
 }
-
